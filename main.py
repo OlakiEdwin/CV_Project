561455171a50525a98cc0c02db0511947ce2d831
Olaki Edwin <olakiedwin@gmail.com>
2024-01-25 13:39:53 +0300

    Adding a heading

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ from docx.shared import Inches
 
 document = Document()
 
+# Profile picture
 document.add_picture(
     'me.jpg',
     width=Inches(2.0)
 )
 
-# name = input('What is your name?')
-# phone_number = input('What is your phone number?')
-# email = input('What is your email?')
+# Name, Phone, Email details
 
-name = 'Olaki Edwin'
-phone_number = '0787499401'
-email = 'olakiedwin@gmail.com'
+name = input('What is your name?')
+phone_number = input('What is your phone number?')
+email = input('What is your email?')
+
+# name = 'Olaki Edwin'
+# phone_number = '0787499401'
+# email = 'olakiedwin@gmail.com'
 
 document.add_paragraph(
     name + ' | ' + phone_number + ' | ' + email)
+
+# About me
+document.add_heading('About me')
+document.add_paragraph(
+    input('Tell me about yourself?')
+)
 
 document.save('cv.docx')
